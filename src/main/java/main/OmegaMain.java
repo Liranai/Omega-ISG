23468409de43bb1b2b5cd22e733cdd0b0c46156b
liranai <GwenvVlokhoven@gmail.com>
2018-09-27 18:06:40 +0200

    Finished GUI, added array to board conversion and started on several AIs

--- src/main/java/main/OmegaMain.java
+++ src/main/java/main/OmegaMain.java
@@ -5,9 +5,9 @@ import ui.OmegaFrame;
 
 public class OmegaMain {
 	
-	public static final int DEBUG = 1;
-	public static final int NUMBER_OF_PLAYERS = 4;
-	public static final int BOARDSIZE = 8;
+	public static final int DEBUG = 2;
+	public static final int NUMBER_OF_PLAYERS = 2;
+	public static final int BOARDSIZE = 10;
 	
 	public enum players {
 		White,
@@ -17,12 +17,27 @@ public class OmegaMain {
 	}
 
     public static void main (String[] args){
-        System.out.println("Hello World");
         Board b = new Board(BOARDSIZE);	
         OmegaLogic logic = new OmegaLogic(b);
         OmegaFrame frame = new OmegaFrame(b, logic);
+        
+        logic.addObserver(frame.getInfoPanel());
+        
         logic.addFrame(frame);
         Thread t = new Thread(logic);
         t.start();
     }
+    
+    //TODO: Multithread move generation??
+    //TODO: Zorbist hash the field/board
+    //TODO: convert the board to primitive types
+    //TODO: UNDO MOVE!!!!!
+    //TODO: Split current model to it's most primitive form and move excess to gui layer
+    //TODO: No MiniMax in the first couple moves, instead evaluate board positions. For example
+    // The outer rims are move valuable since the chances of 'killing' a group are slimmer. Additionally,
+    // you need to protect a group once it only had 1 spot between two groups. On the other hand, using this
+    // you can kill groups of the opponent like this. Therefor, try to always have 2 fields distance between two groups.
+    // ADDITIONALLY: groups of size 1 are USELESS. Make them 2 or bigger AT LEAST
+    // consequently, try to put he opponent's stones in groups of 1.
+    // Change the moveordering to use two individual values instead of just a combined one.
 }
--- src/main/java/ui/OmegaFrame.java
+++ src/main/java/ui/OmegaFrame.java
@@ -2,53 +2,47 @@ package ui;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import lombok.Getter;
 import model.Board;
 import model.Field;
 
+@Getter
 public class OmegaFrame extends JFrame{
 
     private static final long serialVersionUID = -2399200433217264450L;
-    private JPanel boardPanel;
-    public static int SQUARESIZE = 55;
-
-    public static BufferedImage Foot_steps;
-    public static BufferedImage Queen_White;
-    public static BufferedImage Queen_Black;
-    public static BufferedImage Arrow;
+    private OmegaGamePanel boardPanel;
+    private OmegaInfoPanel infoPanel;
 
     public OmegaFrame(Board board, MouseListener listener) {
         super("MainFrame");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(true);
-
-        // TODO: Correct screensize
-        // setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
         setUndecorated(true);
-
-//        try {
-//            // TODO: load transparent files
-//            Foot_steps = ImageIO.read(new File("footsteps3.gif"));
-//            Queen_White = ImageIO.read(new File("white_queen.gif"));
-//            Queen_Black = ImageIO.read(new File("black_queen.gif"));
-//            Arrow = ImageIO.read(new File("arrowCCW.gif"));
-//        } catch (IOException ex) {
-//            ex.printStackTrace();
-//        }
 
         setLayout(new BorderLayout());
         boardPanel = new OmegaGamePanel(board);
+
         boardPanel.addMouseListener(listener);
-        add(boardPanel, BorderLayout.CENTER);
-        setMinimumSize(new Dimension(boardPanel.getMinimumSize()));
+        infoPanel = new OmegaInfoPanel();
+        
+        System.out.println("BPx: " + boardPanel.getMinimumSize().width + " BPy: " + boardPanel.getMinimumSize().height);
+        System.out.println("BIx: " + infoPanel.getMinimumSize().width + " BIy: " + infoPanel.getMinimumSize().height);
+        
+//        setMinimumSize(new Dimension((boardPanel.getMinimumSize().width + infoPanel.getMinimumSize().width), boardPanel.getMinimumSize().height));
+
+        boardPanel.setPreferredSize(boardPanel.getMinimumSize());
+        infoPanel.setPreferredSize(infoPanel.getMinimumSize());
+        
+        
+        add(boardPanel, BorderLayout.WEST);
+        add(infoPanel, BorderLayout.EAST);
         
         addComponentListener(new ComponentListener() {
             public void componentResized(ComponentEvent evt) {
@@ -82,5 +76,6 @@ public class OmegaFrame extends JFrame{
         pack();
         setVisible(true);
         setLocationRelativeTo(null);
+        System.out.println(this.getSize());
     }
 }
